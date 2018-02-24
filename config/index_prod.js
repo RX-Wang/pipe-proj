0dd46578f28107031022f2d1ed1e1bb7b1e6aee2
wangxuquan.byh <wangxuquan.byh@meituan.com>
2018-02-24 18:22:53 +0800

    修改：生产环境项目根路径

--- config/index_prod.js
+++ config/index_prod.js
@@ -24,6 +24,6 @@ module.exports = {
         cookie_name             : 'wanTongGuanYe',
         company_name            : '万通管业',
         company_full_name       : '招远万通管业有限公司',
-        webRoot                 : 'http://XXX:3001' //正式环境
+        webRoot                 : 'http://wtgy.loveruoxi.com' //正式环境
     }
 };
