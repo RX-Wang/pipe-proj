ca4752db5344cd304a1834c96bd68797f6e5166f
wangxuquan.byh <wangxuquan.byh@meituan.com>
2018-02-24 17:27:41 +0800

    修改：启动脚本

--- run.sh
+++ run.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 PROJ_PATH="$(pwd)"
 PROJ_NAME="pipe-proj"
-mv $(PROJ_PATH)/config/index_prod.js $(PROJ_PATH)/config/index.js
-if [ ! -d "$(PROJ_PATH)/logs/" ];then
-mkdir $(PROJ_PATH)/logs/
+cd ${PROJ_PATH}
+echo 'pulling from github'
+git pull
+
+mv ${PROJ_PATH}/config/index_prod.js ${PROJ_PATH}/config/index.js
+if [ ! -d "${PROJ_PATH}/logs/" ];then
+mkdir ${PROJ_PATH}/logs/
 fi
 npm i
-pm2 stop $(PROJ_NAME)
-pm2 delete $(PROJ_NAME)
-pm2 start $(PROJ_PATH)/bin/www
+pm2 stop ${PROJ_NAME}
+pm2 delete ${PROJ_NAME}
+pm2 start ${PROJ_PATH}/bin/www
