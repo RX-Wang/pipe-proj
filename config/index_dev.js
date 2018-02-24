ff5d0bc2a15c85575a330fe2cafd0f018ff29a4b
wangxuquan.byh <wangxuquan.byh@meituan.com>
2018-02-24 18:18:15 +0800

    修改：项目启动端口

--- config/index_dev.js
+++ config/index_dev.js
@@ -25,6 +25,5 @@ module.exports = {
         company_name            : '万通管业',
         company_full_name       : '招远万通管业有限公司',
         webRoot                 : 'http://localhost:3001' // 本地环境
-        // webRoot                 : 'http://XXX:3001', // 正式环境
     }
 };
