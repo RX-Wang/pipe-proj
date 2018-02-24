c35148522a2ec2a203d92d8edbcb33d5f6910598
wangxuquan.byh <wangxuquan.byh@meituan.com>
2018-02-24 17:22:57 +0800

    修改：启动脚本

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 PROJ_PATH="$(pwd)"
 PROJ_NAME="pipe-proj"
-mv PROJ_PATH/config/index_prod.js PROJ_PATH/config/index.js
-pm2 stop PROJ_NAME
-pm2 delete PROJ_NAME
-pm2 start PROJ_PATH/bin/www
+mv $(PROJ_PATH)/config/index_prod.js $(PROJ_PATH)/config/index.js
+if [ ! -d "$(PROJ_PATH)/logs/" ];then
+mkdir $(PROJ_PATH)/logs/
+fi
+npm i
+pm2 stop $(PROJ_NAME)
+pm2 delete $(PROJ_NAME)
+pm2 start $(PROJ_PATH)/bin/www
